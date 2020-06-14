35febd92239a4fde9dd3e739dfcb56df22046524
Abhijeet Jadhav <P10473326@capita.co.uk>
2020-06-14 22:04:16 +0530

    react ionic project first step completed

---
+++ Episode 01_02/hello-world/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        Hello World
+      </header>
+      <main>
+        <IonButton>Click Me</IonButton>
+      </main>
+    </div>
+  );
+}
+
+export default App;
