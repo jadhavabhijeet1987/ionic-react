530606276bd6c4cd1fc2053c36ca52819ba4674f
Abhijeet Jadhav <P10473326@capita.co.uk>
2020-06-15 15:32:59 +0530

    toast added

--- Episode 01_02/hello-world/src/App.js
+++ Episode 01_02/hello-world/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { IonButton } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonButton, IonToast } from '@ionic/react';
 import './App.css';
 
 function App() {
+  const [showToast, setShowState] = useState(false);
+
+  const handleClick = () => {
+    setShowState(true);
+    setTimeout(() => {
+      setShowState(false);
+    }, 1500);
+  }
+
   return (
     <div >
       <header>
         Hello World
       </header>
       <main>
-        <IonButton>Click Me</IonButton>
+        <IonButton onClick={handleClick}>Click Me</IonButton>
+        <IonToast isOpen={showToast} message="Hello World!"></IonToast>
       </main>
     </div>
   );
